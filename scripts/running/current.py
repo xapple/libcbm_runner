3a737a459f73b88d11e06262a8a2332380332f41
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-10-15 12:03:11 +0200

    Simpler call to runners

--- scripts/running/current.py
+++ scripts/running/current.py
@@ -6,13 +6,12 @@ A script to run the current feature that is being developed and test it.
 
 Typically you would run this file from a command line like this:
 
-     ipython3.exe -i -- /deploy/libcbm_runner/scripts/running/current.py
+     ipython3 -i -- ~/deploy/libcbm_runner/scripts/running/current.py
 """
 
 # Built-in modules #
 
 # Third party modules #
-from tqdm import tqdm
 
 # First party modules #
 
@@ -21,7 +20,8 @@ from libcbm_runner.core.continent import continent
 
 ################################################################################
 scenario = continent.scenarios['historical']
-runners  = [rs[-1] for k,rs in scenario.runners.items() if k=='ZZ']
-runner   = runners[0]
+runner   = scenario.runners['ZZ'][-1]
 
 runner.run()
+print(runner.simulation.results)
+print(runner.simulation.inventory)
