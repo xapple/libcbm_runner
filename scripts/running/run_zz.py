e6ca8005fac2b57c96d3dcf90fa0a40ad1ad271e
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-10-17 17:20:10 +0200

    A seperate script for running ZZ

---
+++ scripts/running/run_zz.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Written by Lucas Sinclair and Paul Rougieux.
+
+JRC Biomass Project.
+Unit D1 Bioeconomy.
+
+A script to run the imaginary ZZ country to test the pipeline.
+
+Typically you would run this file from a command line like this:
+
+     ipython3 -i -- ~/deploy/libcbm_runner/scripts/running/run_zz.py
+"""
+
+# Built-in modules #
+
+# Third party modules #
+
+# First party modules #
+
+# Internal modules #
+from libcbm_runner.core.continent import continent
+
+################################################################################
+scenario = continent.scenarios['historical']
+runner   = scenario.runners['ZZ'][-1]
+
+runner.run()
+print(runner.simulation.results)
+print(runner.simulation.inventory)
