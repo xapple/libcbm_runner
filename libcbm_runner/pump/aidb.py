f2e01b101d825ecb87ae1ea9b8a96ca6b5e471ee
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-07-20 01:44:14 +0200

    Add method symlink_single_aidb

--- libcbm_runner/pump/aidb.py
+++ libcbm_runner/pump/aidb.py
@@ -9,10 +9,12 @@ Unit D1 Bioeconomy.
 """
 
 # Built-in modules #
+import os
 
 # Third party modules #
 
 # First party modules #
+from autopaths.dir_path import DirectoryPath
 from autopaths.auto_paths import AutoPaths
 
 # Internal modules #
@@ -22,7 +24,12 @@ class AIDB(object):
     """
     This class will provide access to the archive index database
     also called 'cbm_defaults' in libcbm.
-    It is an SQLite3 database.
+    It is an SQLite3 database that weighs approx 18 MiB.
+
+    To symlink the single test database to all countries do the following:
+
+        >>> from libcbm_runner.core.continent import continent
+        >>> for country in continent: country.aidb.symlink_single_aidb()
     """
 
     all_paths = """
@@ -34,3 +41,20 @@ class AIDB(object):
         self.parent = parent
         # Directories #
         self.paths = AutoPaths(self.parent.data_dir, self.all_paths)
+
+    def symlink_single_aidb(self):
+        """
+        During development, and for testing purposes we have a single AIDB
+        that all countries can share and that is found in another repository.
+        """
+        # Where is the data, default case #
+        aidb_repo = DirectoryPath("~/repos/libcbm_aidb/")
+        # But you can override that with an environment variable #
+        if os.environ.get("AIDB_REPO"):
+            aidb_repo = DirectoryPath(os.environ['AIDB_REPO'])
+        # Check it exists #
+        source = aidb_repo + 'aidb.db'
+        assert source
+        # Symlink #
+        destin = self.paths.aidb
+        source.link_to(destin)
