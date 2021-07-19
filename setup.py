6d3c360dfd230b0460823e6cff2f0a410876f95f
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-07-19 16:35:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,38 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+"""
+Written by Lucas Sinclair and Paul Rougieux.
+
+JRC biomass Project.
+Unit D1 Bioeconomy.
+"""
+
+# Imports #
+from setuptools import setup, find_namespace_packages
+from os import path
+
+# Load the contents of the README file #
+this_dir = path.abspath(path.dirname(__file__))
+readme_path = path.join(this_dir, 'README.md')
+with open(readme_path, encoding='utf-8') as handle: readme = handle.read()
+
+# Call setup #
 setup(
-        name             = 'libcbm_runner',
-        version          = '0.1.1',
-        description      = 'libcbm_runner is a python package for running carbon budget simulations.',
-        long_description = open('README.md').read(),
-        long_description_content_type = 'text/markdown',
-        license          = 'MIT',
-        url              = 'https://github.com/xapple/libcbm_runner',
-        author           = 'Lucas Sinclair',
-        author_email     = 'lucas.sinclair@me.com',
-        packages         = find_packages(),
-        install_requires = ['autopaths', 'plumbing', 'pymarktex', 'pandas', 'pystache',
-                            'simplejson', 'matplotlib', 'tqdm'],
-    )
+    name             = 'libcbm_runner',
+    version          = '0.1.2',
+    description      = 'libcbm_runner is a python package for running carbon'
+                       ' budget simulations.',
+    license          = 'MIT',
+    url              = 'https://github.com/xapple/libcbm_runner',
+    author           = 'Lucas Sinclair',
+    author_email     = 'lucas.sinclair@me.com',
+    packages         = find_namespace_packages(),
+    install_requires = ['autopaths>=1.5.2', 'plumbing>=2.10.4',
+                        'pymarktex>=1.4.6', 'pandas', 'pystache', 'simplejson',
+                        'matplotlib', 'tqdm'],
+    python_requires  = ">=3.8",
+    long_description = readme,
+    long_description_content_type = 'text/markdown',
+    include_package_data = True,
+)
