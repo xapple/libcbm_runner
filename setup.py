77ce25a08d57ca432a404db5628405f43bf71886
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-07-21 23:57:02 +0200

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(readme_path, encoding='utf-8') as handle: readme = handle.read()
 # Call setup #
 setup(
     name             = 'libcbm_runner',
-    version          = '0.1.2',
+    version          = '0.2.0',
     description      = 'libcbm_runner is a python package for running carbon'
                        ' budget simulations.',
     license          = 'MIT',
@@ -30,7 +30,7 @@ setup(
     packages         = find_namespace_packages(),
     install_requires = ['autopaths>=1.5.2', 'plumbing>=2.10.4',
                         'pymarktex>=1.4.6', 'pandas', 'pystache', 'simplejson',
-                        'matplotlib', 'tqdm'],
+                        'matplotlib', 'tqdm', 'p_tqdm'],
     python_requires  = ">=3.8",
     long_description = readme,
     long_description_content_type = 'text/markdown',
