ad39200a1e15b4a07158537c00c32247b7f51c51
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-07-23 19:58:41 +0200

    Working on pools comparision

--- scripts/comparison/pools.py
+++ scripts/comparison/pools.py
@@ -40,6 +40,8 @@ class ComparisonRunner(object):
     def __init__(self, cbmcfs3_country):
         # Main attributes #
         self.cbmcfs3_country = cbmcfs3_country
+        # Shortcuts #
+        self.iso2_code = cbmcfs3_country.iso2_code
 
     #----------------------------- Properties --------------------------------#
     @property_cached
@@ -47,31 +49,56 @@ class ComparisonRunner(object):
         """The matching libcbm country object."""
         return libcbm_continent.countries[self.cbmcfs3_country.iso2_code]
 
+    @property
+    def title(self):
+        msg = "# %s (%s)\n"
+        msg = msg % (self.cbmcfs3_country.country_name, self.iso2_code)
+        msg = msg + "### Comparing libcbm -vs- cbmcfs3 \n\n"
+        return msg
+
+    #--------- Scenarios ----------#
+    @property
+    def scen_cbmcfs3(self):
+        return cbmcfs3_continent.scenarios['historical']
+
+    @property
+    def scen_libcbm(self):
+        return libcbm_continent.scenarios['historical']
+
     #---------- Runners -----------#
     @property
     def runner_cbmcfs3(self):
-        return self.runner_cbm3.post_processor.pool_indicators_long
+        return self.scen_cbmcfs3[self.iso2_code][-1]
 
     @property
     def runner_libcbm(self):
-        return self.runner_cbm3.post_processor.pool_indicators_long
+        return self.scen_libcbm[self.iso2_code][-1]
 
-    #---------- Pools -----------#
+    #----------- Pools ------------#
     @property
     def pools_cbmcfs3(self):
-        return self.runner_cbm3.post_processor.pool_indicators_long
+        # Load #
+        post = self.runner_cbmcfs3.post_processor
+        result = post.pool_indicators_long
+        # Return #
+        return result
 
     @property
     def pools_cbmcfs3(self):
-        return self.runner_cbm3.post_processor.pool_indicators_long
+        # Load #
+        result = self.runner_libcbm.output['pools']
+        id_vars = ['identifier', 'timestep', 'Input']
+        # Unpivot #
+        result = result.melt(id_vars    = id_vars,
+                             var_name   = 'pool',
+                             value_name = 'tc')
+        # Return #
+        return result
 
     #------------------------------- Methods ---------------------------------#
     def __call__(self):
-        id_vars = ['identifier', 'timestep', 'Input']
-        pools_libcbm = pools_libcbm_wide.melt(id_vars    = id_vars,
-                                              var_name   = 'pool',
-                                              value_name = 'tc')
-
+        print(self.title)
+        
 
 ###############################################################################
 if __name__ == '__main__':
