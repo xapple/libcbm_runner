30bbdf89e34f94da31ab1ff686d2a075dd77df77
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-08-11 19:52:51 +0200

    Rename all classifiers to snake_case in orig data

---
+++ scripts/conversion/rename_all_classifiers.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+A script to harmonize the names of classifiers in every country to snake_case.
+"""
+
+# Built-in modules #
+
+# Third party modules #
+import pandas
+from tqdm import tqdm
+
+# First party modules #
+
+# Internal modules #
+from libcbm_runner.core.continent import continent
+
+###############################################################################
+class ClassifierRenamer(object):
+    """
+    `Forest type` becomes `forest_type`.
+
+    """
+
+    mapping = {
+        'Status'                         : 'status',
+        'Forest type'                    : 'forest_type',
+        'Region'                         : 'region',
+        'Management type'                : 'mgmt_type',
+        'Management strategy'            : 'mgmt_strategy',
+        'Climatic unit'                  : 'climate',
+        'Conifers/Broadleaves'           : 'con_broad',
+        'Site index'                     : 'site_index',
+        'Simulation period (for yields)' : 'growth_period',
+    }
+
+    def __init__(self, country):
+        # Default attributes #
+        self.country = country
+
+    def __call__(self):
+        path = self.country.orig_data.paths.classifiers
+        df = pandas.read_csv(str(path))
+        df['name'] = df['name'].replace(self.mapping)
+        df.to_csv(str(path), index=False, float_format='%g')
+
+###############################################################################
+if __name__ == '__main__':
+    renamers = [ClassifierRenamer(c) for c in continent]
+    for renamer in tqdm(renamers): renamer()
